0638f9f6ef4328be749b31640480d8e5f840fcb5
Le Manh Duong (VTI.D5) <duong.lemanh@vti.com.vn>
2024-03-11 16:32:21 +0700

    first commit

---
+++ src/components/MyComponent/MyComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <button @click="clickIncrement">count is {{ count }}</button>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const count = ref(0);
+
+function clickIncrement() {
+  count.value++;
+}
+</script>
+<style lang="css" src="./index.css"></style>
